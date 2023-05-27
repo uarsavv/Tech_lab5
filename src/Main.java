9d2cd6db1a7eb1c6fc598bd68f2e21c5b52cd02a
Iva <uarovaiva@gmail.com>
2023-05-27 18:04:28 +0300

    change method

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         SQLManager sqlMan = new SQLManager();
         sqlMan.Conn();
         sqlMan.ReadDB();
-//        sqlMan.DeleteByID(25);
+        //sqlMan.DeleteByID(22);
         sqlMan.WriteDB("Special Feel", "Twice", 20210000L, 15.5F, 14, 1);
         System.out.println(sqlMan.FindById(1));
         System.out.println(sqlMan.FindByArtist("Sting"));
